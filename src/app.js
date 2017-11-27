25ac31f4b5e6acd92f966bace4bc2b0ddc78fb96
Xiaoye Hu <ben.huxiaoye@gmail.com>
2017-11-27 14:56:09 -0800

    update ipv6 logic

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var ip_checker = require("is-ip"); //to check ipv4 or ipv6
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -13,32 +14,33 @@ var cache_capacity = 50;
 var node_list = [];
 
 app.post("/api/nodes", function(req, res) {
+
   console.log("entered route");
-  //deep copy request headers
-  var new_node = JSON.parse(JSON.stringify(req.body));
 
   //check incomplete information
-  if (!new_node.username || !new_node.public_key) {
-    console.log("Missing username or pk")
+  if (!req.body.username || !req.body.public_key) {
+    console.log("Missing username or pk");
     res.status(400);
     return res.json({
       success: false,
-      message: "Missing node information. Please provide id, username, and public_key."
+      message: "Missing node information. Please provide username and public_key."
     });
   }
-
+  //deep copy request body
+  var new_node = JSON.parse(JSON.stringify(req.body));
   //deep copy node list for sending response
   var node_list_copy = JSON.parse(JSON.stringify(node_list));
+  console.log(node_list);
 
-  //enqueue node information
-  let new_node_obj = {};
-  new_node_obj.ip = req.connection.remoteAddress;
-  new_node_obj.port = req.connection.remotePort;
-  new_node_obj.username = new_node.username;
-  new_node_obj.public_key = new_node.public_key;
-  node_list.push(new_node_obj);
+  //analyze ip address
+  if (ip_checker.v6(req.connection.remoteAddress)) {
+    new_node.address = "[" + req.connection.remoteAddress + "]:" + req.connection.remotePort;
+  } else {
+    new_node.address = req.connection.remoteAddress + ":" + req.connection.remotePort;
+  }
 
-  //trim cache
+  //enqueue node information, trim cache
+  node_list.push(new_node);
   while (node_list.length > cache_capacity) {
     node_list.shift();
   }
